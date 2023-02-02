eb1311982c204d6ab6d9278647a8d3b1906f0a32
Jinsol <jinsol0323@naver.com>
2023-02-02 22:02:55 +0900

    "ch6 학습중"

--- lecture/server1.js
+++ lecture/server1.js
@@ -15,3 +15,14 @@ server.on("listening", (error) => {
 server.on("error", (error) => {
   console.error(error);
 });
+
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/html; charset=utf-8 " });
+    res.write("<h1>Hello World!</h1>");
+    res.write("<p>Hello Node!</p>");
+    res.end("<p>Hello Server!</p>");
+  })
+  .listen(8081, () => {
+    console.log("8081 포트에서 서버 대기 중입니다!");
+  });
